73a4aa4da71c9c0a80200bfb315c69e960196b26
Yuki Jung <yukiej@gmail.com>
2018-09-25 13:40:29 -0700

    Add Grub and Bee class using ES6

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,11 @@
-class Bee {
-  // TODO..
+class Bee extends Grub {
+  constructor(age, color,food){
+    super(age, color, food);
+    this.job = 'Keep on growing';
+    this.age = 5; 
+    this.color = 'yellow';
+  }
 };
+
+
+
