aa33afe352cd9b7f0313ea91dfc580c3e3a4d0b5
Yuki Jung <yukiej@gmail.com>
2018-09-25 14:02:16 -0700

    Add RetiredForagerBee class using ES6

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,17 @@
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee {
+  constructor(age, color, food) {
+    super(age, color, food);
+    this.age = 40;
+    this.job = 'gamble';
+    this.canFly = false;
+    this.color = 'grey'; 
+  }
+
+  forage() {
+    return 'I am too old, let me play cards instead';
+  }
+
+  gamble(treasure) {
+    this.treasureChest.push(treasure);
+  }
 };
