773003e80f6576069a22034f27742dc2fe3a2c8e
Yuki Jung <yukiej@gmail.com>
2018-09-25 13:55:34 -0700

    Add ForagerBee and HoneyMakerBee classes using ES6

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,15 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee{
+  constructor(age, color, food) {
+    super(age, color, food);
+    this.age = 10;
+    this.job = 'make honey'
+    this.honeyPot = 0;
+  }
+  makeHoney() {
+    this.honeyPot++;
+  }
+
+  giveHoney() {
+    this.honeyPot--;
+  }
 };
